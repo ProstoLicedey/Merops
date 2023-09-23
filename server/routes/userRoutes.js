637604bb9b4a93fe5f1910f7d75ea889636a8b90
Михаил Максимов <mi2004ha@mail.ru>
2023-09-23 03:31:11 +0300

    Инциализация React

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const Router = require('express')
 const  router = new Router()
 
-const {User} = require("../models/models");
+//const {User} = require("../models/models");
 
 
 router.post('/registration', )
